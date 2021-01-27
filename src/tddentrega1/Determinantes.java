71cbf0c484329ac0fed7a0663fedaed56d3fc60c
Javier <javierclavijog@gmail.com>
2021-01-27 13:31:54 +0100

    Versión 2.0 -- Corregir test, cambiar tipo int a double en Determinantes

--- src/tddentrega1/Determinantes.java
+++ src/tddentrega1/Determinantes.java
@@ -3,19 +3,19 @@ package tddentrega1;
 import java.util.Arrays;
 
 public class Determinantes {
-    public static int calcularDeterminantes(int[][] array) {
+    public static double calcularDeterminantes(double[][] array) {
         if (array.length != 3 || Arrays.stream(array).anyMatch(a -> a.length != 3)) {
             throw new IllegalArgumentException("El array introducido no tiene las dimensiones correctas");
         }
-        final int A = array[0][0];
-        final int B = array[0][1];
-        final int C = array[0][2];
-        final int D = array[1][0];
-        final int E = array[1][1];
-        final int F = array[1][2];
-        final int G = array[2][0];
-        final int H = array[2][1];
-        final int I = array[2][2];
+        final double A = array[0][0];
+        final double B = array[0][1];
+        final double C = array[0][2];
+        final double D = array[1][0];
+        final double E = array[1][1];
+        final double F = array[1][2];
+        final double G = array[2][0];
+        final double H = array[2][1];
+        final double I = array[2][2];
 
         return (A * E * I + B * F * G + C * D * H) - (G * E * C + H * F * A + I * D * B);
     }
--- test/tddentrega1/DeterminantesTest.java
+++ test/tddentrega1/DeterminantesTest.java
@@ -1,6 +1,10 @@
 package tddentrega1;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,27 +12,26 @@ class DeterminantesTest {
 
     @Test
     void calcularDeterminantes() {
-        // Test A
-        int esperado = 0;
-        int[][] testArray = new int[][]{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
-        int resultado = Determinantes.calcularDeterminantes(testArray);
+        double esperado = 0;
+        double[][] testArray = new double[][]{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+        double resultado = Determinantes.calcularDeterminantes(testArray);
         assertEquals(resultado, esperado);
 
         // Test B
-        esperado = 1;
-        testArray = new int[][]{{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
+        esperado = 0;
+        testArray = new double[][]{{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
         resultado = Determinantes.calcularDeterminantes(testArray);
         assertEquals(resultado, esperado);
 
         //Test C
         esperado = -3;
-        testArray = new int[][]{{2, 1, 3}, {-1, 1, 0}, {-2, 4, 1}};
+        testArray = new double[][]{{2, 1, 3}, {-1, 1, 0}, {-2, 4, 1}};
         resultado = Determinantes.calcularDeterminantes(testArray);
         assertEquals(resultado, esperado);
 
         //Test D
         esperado = 11;
-        testArray = new int[][]{{2, 3, -1}, {0, 1 / 2, -1}, {-2, 1, 4}};
+        testArray = new double[][]{{2, 3, -1}, {0, 1.0 / 2.0, -1}, {-2, 1, 4}};
         resultado = Determinantes.calcularDeterminantes(testArray);
         assertEquals(resultado, esperado);
     }
